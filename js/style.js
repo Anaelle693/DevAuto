42acd6f9617e9673f4c3cf6951b2c0f2ba0f423a
Anaelle693 <anaellev@icloud.com>
2021-04-23 09:49:22 +0200

    projet

---
+++ js/style.js
@@ -0,0 +1,17 @@
+// $( document ).ready(function() {
+    
+//     // $("container>:first-child").hide();
+
+//     // $(".a5").html("<div>Coucou</div>");
+//     // $("button").css("background-color", "red");
+//     $("header>a>img").click(function(){
+//         $("img").css("background-image", "url(../img/logo_audi.jpg)");
+//     });
+    
+// });
+// $( document ).ready(function() {
+//     $("#logo").click(function(){
+//         $("#logo").attr("src","../img/23571.svg");
+        
+//     });
+// });
